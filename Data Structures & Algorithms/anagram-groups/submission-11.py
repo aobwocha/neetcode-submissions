b41faec5d18cdd6d8db820ccad4028b7eb12a2c3
aobwocha <zeros-faddish.0t@icloud.com>
2026-08-13 16:53:27 -0600

    Add: anagram-groups - submission-11

---
+++ Data Structures & Algorithms/anagram-groups/submission-11.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        grouped_words = collections.defaultdict(list)
+
+        for word in strs:
+            key = [0] * 26
+            for char in word:
+                key[ord(char) - ord('a')] += 1
+            grouped_words[tuple(key)].append(word)
+        
+        return list(grouped_words.values())
