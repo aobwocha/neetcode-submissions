6a9e00f4aaa0bef84c477ff819b0933f85a98189
aobwocha <zeros-faddish.0t@icloud.com>
2026-08-18 20:21:16 -0600

    Add: minimum-stack - submission-9

---
+++ Data Structures & Algorithms/minimum-stack/submission-9.py
@@ -0,0 +1,31 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = list()
+        self.min = None
+
+    def push(self, val: int) -> None:
+        if self.min is not None:
+            self.stack.append(val - self.min)
+            self.min = min(self.min, val)
+        else:
+            self.stack.append(0)
+            self.min = val
+
+    def pop(self) -> None:
+        res = self.stack.pop()
+        if res < 0:
+            self.min -= res
+        
+        if not self.stack:
+            self.min = None
+
+    def top(self) -> int:
+        res = self.stack[-1]
+        if res >= 0:
+            return res + self.min
+        else:
+            return self.min
+
+    def getMin(self) -> int:
+        return self.min
